7eaa5915817d098a8f6ca47a65e05d129bc51a14
Jordan Baczuk <Jordan@kilfetech.com>
2017-12-03 22:29:39 -0700

    new pre-commit

--- pre-commit.sh
+++ pre-commit.sh
@@ -11,16 +11,15 @@ STASH_NAME="pre-commit-$(date +%s)"
 git stash save -q --keep-index $STASH_NAME
 
 # Test prospective commit
-# FILES_PATTERN='\.(js|coffee)(\..+)?$'
 echo "\nChecking commit...\n"
 FORBIDDEN='[a-zA-Z0-9|$-\/|:-?|{-~|]{32}'
 EXCLUDE='.lock'
-git diff --cached --name-only | \
-    grep -v $EXCLUDE | \
-    GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n -iE $FORBIDDEN \
-    && echo 'COMMIT REJECTED Found possible secret keys. Please remove them before committing or use --no-verify to ignore and commit anyway' \
-    && echo 'If this helped you, consider donating BTC to: 183J9CYci5Xbe3YXct1BHyRjyxH89QiUCc I had .25 BTC stolen just trying to earn it back ;)' \
-    && exit 1
+COMMAND="git diff --cached --name-only | grep -v $EXCLUDE | GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n -iE $FORBIDDEN"
+FAIL_MESSAGE = "COMMIT REJECTED Found possible secret keys. Please remove them before committing or use --no-verify to ignore and commit anyway.\n If this helped you, consider donating Bitcoin to: 183J9CYci5Xbe3YXct1BHyRjyxH89QiUCc I had .25 BTC stolen :( just trying to earn it back ;)"
+
+if $($COMMAND); then
+  echo $FAIL_MESSAGE && exit 1
+fi
 
 STASHES=$(git stash list)
 if [[ $STASHES == "$STASH_NAME" ]]; then
